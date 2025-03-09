5b243c142a2ddcf5f7e07ae941b145552239f03a
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-03-09 14:56:37 +0530

    user model + signup backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,21 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
 import User from '../models/user';
 const authRouter = express.Router();
 
+// -------------Registration of a User into Database--------------
+
 authRouter.post('/signup', async (req, res) => {
     try {
-        const { name, email, password, userId } = req.body;
-        const hashPassword = await bcrypt
+        const { name, email, password } = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            name,
+            email,
+            password: hashPassword,
+        })
+        await user.save();
+        res.send("Sign Up Succesfully");
     } catch (err) {
         res.status(400).send("Could'nt Sign Up" + err.message)
     }
