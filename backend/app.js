e3338e1bfff788edc756454ca2818810789dce3c
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-03-13 01:02:10 +0530

    dependencies & signup router initialized

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const dbConnection = require('./config/database');
 
 dbConnection()
     .then(() => console.log('DB connected successfully'))
     .catch((err) => console.error(`DB connection error: ${err.message}`));
+
+
+app.use(cors({
+    origin: 'http://localhost:5173', // React frontend URL
+    credentials: true // Allow cookies to be passed
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Register from './pages/Register'
+
+const router = createBrowserRouter([
+  {
+    path: `/`,
+    element: <App />,
+    children: [
+      // {
+      //   path: `/`,
+      //   element: <Home />
+      // }
+      // ,
+      {
+        path: `/users/signup`,
+        element: (
+          // <AuthLayout authentication={false}>
+          <Register />
+          // </AuthLayout>
+        )
+      },
+      {
+        path: `/login`,
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      }
+
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
 )
